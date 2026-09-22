e11f19ed30d26c465bb4fa9ea2e8f46d1a44773b
enidpublicrecord <57514486+enidpublicrecord@users.noreply.github.com>
2026-09-22 11:11:58 -0500

    Add files via upload

--- explore-test/sw.js
+++ explore-test/sw.js
@@ -1,4 +1,4 @@
-const CACHE='epr-explore-test-v2.0-20260922';
+const CACHE='epr-explore-test-v2.1-logo-20260922';
 const SHELL=['/explore-test/','/explore-test/index.html','/explore-test/assets/explore.css','/explore-test/assets/explore.js','/explore-test/assets/epr-logo.png','/explore-test/manifest.webmanifest','/explore-test/assets/icon-180.png','/explore-test/assets/icon-192.png','/explore-test/assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('epr-explore-test-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
